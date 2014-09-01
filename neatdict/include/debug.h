c902e4db4626da32801f32f3607174bf9ff98f0d
nil0x42 <nil0x42@gmail.com>
2014-09-01 22:31:47 +0200

    Fix line bug in DLOG() debug function

--- neatdict/include/debug.h
+++ neatdict/include/debug.h
@@ -9,18 +9,18 @@
 #  include <stdio.h>
 #  include <stdarg.h>
 
-static void dlog_printf(const char *fmt, ...)
+static void dlog_printf(const char *file, int line, const char *fmt, ...)
 {
     va_list ap;
 
     va_start(ap, fmt);
-    dprintf(STDERR_FILENO, "DLOG(%s:%d): ", __FILE__, __LINE__);
+    dprintf(STDERR_FILENO, "DLOG(%s:%d): ", file, line);
     vdprintf(STDERR_FILENO, fmt, ap);
     write(STDERR_FILENO, "\n", 1);
     va_end(ap);
 }
 
-#  define DLOG(...) (dlog_printf(__VA_ARGS__))
+#  define DLOG(...) (dlog_printf(__FILE__, __LINE__, __VA_ARGS__))
 # else
 #  define DLOG(...)
 # endif
