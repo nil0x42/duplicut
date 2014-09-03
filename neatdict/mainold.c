76e008ab7489e3bc2338fa05f48886d82823c99b
nil0x42 <nil0x42@gmail.com>
2014-09-03 19:58:45 +0200

    Add old main file on repo for later use

---
+++ neatdict/mainold.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "definitions.h"
+#include "debug.h"
+
+t_chunk         *g_chunks = NULL;
+
+void            print_chunks(t_chunk *chunk)
+{
+    while (chunk != NULL)
+    {
+        printf("\n");
+        printf("==========================\n");
+        printf("fd   = '%d'\n",         chunk->fd);
+        printf("name = '%s'\n",        chunk->name);
+        printf("ptr  = '%p'\n",         chunk->ptr);
+        printf("size = '%ld'\n",        chunk->size);
+        printf("next = '%p'\n",         chunk->next);
+        printf("-------------------------'\n");
+        printf("file.fd     = '%d'\n",   chunk->file.fd);
+        printf("file.name   = '%s'\n",   chunk->file.name);
+        printf("file.offset = '%ld'\n",  chunk->file.offset);
+        printf("file.size   = '%ld'\n",  chunk->file.size);
+        chunk = chunk->next;
+    }
+}
+
+int             main(int argc, char **argv)
+{
+    t_chunk     *chunklist;
+    int         i;
+
+    chunklist = NULL;
+    if (argc < 2)
+    {
+        dprintf(STDERR_FILENO, "Usage: %s [<wordlist>] ...\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+    i = 0;
+    while (++i != argc)
+    {
+        DLOG("chunkify_file(%s)", argv[i]);
+        chunkify_file(argv[i]);
+    }
+    print_chunks(chunklist);
+    return (EXIT_SUCCESS);
+}
