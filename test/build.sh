3ecaaf4835ce094f00eb31c6a3db5a445d601f94
nil0x42 <nil0x42@gmail.com>
2014-12-18 01:03:53 +0100

    [UNITTEST] Update duplicut builder for tests

--- test/build.sh
+++ test/build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Build a release version for use
+# with existing tests.
+
+# Usage:
+#   ./build.sh
+#   ./build.sh <ARCH>
+#
+# Example:
+#   ./build.sh "x64"
+
 set -ve
 
-echo "CC=$CC"
+CC="$CC"
+FLAGS="$FLAGS"
+
 make fclean
 make
 make fclean
