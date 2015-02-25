485be0b1502810b55a8917a130f08c253aa1ca49
nil0x42 <nil0x42@gmail.com>
2015-02-25 06:33:26 +0100

    Improve hmap.c, make delete_hmap atexit() descruct

--- include/hmap.h
+++ include/hmap.h
@@ -4,6 +4,5 @@
 # include "line.h"
 
 t_line  *create_hmap(void);
-void    delete_hmap(void);
 
 #endif /* ! HMAP_H */
--- src/hmap.c
+++ src/hmap.c
@@ -5,24 +5,31 @@
 #include "error.h"
 
 
+static void delete_hmap(void)
+{
+    if (g_vars.hmap != NULL)
+    {
+        free(g_vars.hmap);
+        g_vars.hmap = NULL;
+    }
+}
+
+
 t_line      *create_hmap(void)
 {
     t_line  *area;
+
+    if (g_vars.hmap != NULL)
+        error("create_hmap(): singleton called twice !");
 
     area = (t_line*) malloc(g_conf.hmap_size * sizeof(t_line));
     if (area == NULL)
     {
         error("cannot malloc() hash map: %s", ERRNO);
     }
+
+    atexit(delete_hmap);
     g_vars.hmap = area;
+
     return (area);
-}
-
-
-void        delete_hmap(void)
-{
-    if (g_vars.hmap != NULL)
-    {
-        free(g_vars.hmap);
-    }
 }
