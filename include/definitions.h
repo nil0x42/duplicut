11c4ea218ee8b52fdbffa3e1dce4948c537e0587
nil0x42 <nil0x42@gmail.com>
2015-02-26 07:42:27 +0100

    Cleanout definitions.h

--- include/definitions.h
+++ include/definitions.h
@@ -1,24 +1,17 @@
-#ifndef DEFINITIONS_H
-# define DEFINITIONS_H
+#pragma once
 
-# define PROGNAME           "duplicut"
-# define PROJECT_VERSION    "0.1a"
-# define PROJECT_URL        "http://github.com/nil0x42/duplicut"
+#define PROGNAME            "duplicut"
+#define PROJECT_VERSION     "0.1a"
+#define PROJECT_URL         "http://github.com/nil0x42/duplicut"
 
 /* max hashmap size (factor based on availabled memory) */
-# define HMAP_MAX_SIZE      (0.2)
+#define HMAP_MAX_SIZE       (0.2)
 
 /* hashmap maximum load factor */
-# define HMAP_LOAD_FACTOR   (0.5)
+#define HMAP_LOAD_FACTOR    (0.5)
 
 /* medium line size for determining hashmap min size */
-# define MEDIUM_LINE_BYTES  (6)
+#define MEDIUM_LINE_BYTES   (6)
 
 /* this char is written at start of lines as a `removal mark` */
-# define DISABLED_LINE      '\0'
-
-
-/* source file: filehandle.c */
-void            check_files(char **array, int len);
-
-#endif /* DEFINITIONS_H */
+#define DISABLED_LINE       '\0'
