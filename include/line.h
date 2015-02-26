53a678537985af7d17e9c156122c9f728be49abe
nil0x42 <nil0x42@gmail.com>
2015-02-26 07:41:09 +0100

    Rewrite line.c && line.h

--- include/line.h
+++ include/line.h
@@ -1,20 +1,20 @@
-#ifndef LINE_H
-# define LINE_H
+#pragma once
 
-# include <stddef.h>
-# include <stdint.h>
-# include "chunk.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "chunk.h"
+
 
 typedef size_t  t_line;
 
-#  define LINE_ISSET(ln)        (ln != 0)
-#  define LINE_SIZE(ln)         ((uint8_t)ln)
-#  define LINE_ADDR(ln)         ((char*)(ln >> 8))
-#  define SET_LINE(ln, ptr, sz) (ln = ((((uintptr_t)ptr) << 8) + (uint8_t)sz))
 
-// line_utils.c
-int         cmp_line(t_line *l1, t_line *l2);
-t_line      *next_line(char *buf_addr, size_t buf_size,
-        t_line *line, size_t *offset);
+#define LINE_ISSET(ln)          (ln != 0)
+#define LINE_SIZE(ln)           ((uint8_t)ln)
+#define LINE_ADDR(ln)           ((char*)(ln >> 8))
+#define SET_LINE(ln, ptr, sz)   (ln = ((((uintptr_t)ptr) << 8) + (uint8_t)sz))
 
-#endif /* LINE_H */
+
+/* source file: line.c */
+bool    get_next_line(t_line *dst, t_chunk *chunk);
+int     cmp_line(t_line *l1, t_line *l2);
--- src/line.c
+++ src/line.c
@@ -2,90 +2,49 @@
 #include "line.h"
 #include "definitions.h"
 #include "debug.h"
+#include "chunk.h"
 #include "config.h"
 
 
-static void     dlog_obj_t_chunk(t_chunk *chunk)
+bool        get_next_line(t_line *dst, t_chunk *chunk)
 {
-    DLOG("------------------------------");
-    DLOG("chunk->tag:           '%d'",  chunk->tag);
-    DLOG("chunk->id:            '%d'",  chunk->id);
-    DLOG("chunk->offset:        '%ld'", chunk->offset);
-    DLOG("chunk->size:          '%ld'", chunk->size);
-    DLOG("");
-    DLOG("chunk->file.fd:       '%d'",  chunk->file.fd);
-    DLOG("chunk->file.name:     '%s'",  chunk->file.name);
-    DLOG("chunk->file.offset:   '%ld'", chunk->file.offset);
-    DLOG("chunk->file.size:     '%ld'", chunk->file.size);
-    DLOG("");
-    DLOG("chunk->map.addr:      '%p'",  chunk->map.addr);
-    DLOG("chunk->map.size:      '%ld'", chunk->map.size);
-    DLOG("");
-    DLOG("chunk->next:          '%p'",  chunk->next);
-    DLOG("------------------------------");
-}
-
-
-t_line      *next_line(char *buf_addr, size_t buf_size,
-        t_line *line, size_t *offset)
-{
-    char    *ptr;
-    char    *addr;
     size_t  size;
-    size_t  delta;
+    char    *next;
     size_t  line_size;
 
-    if (*offset >= buf_size)
+    size = chunk->endptr - chunk->ptr;
+    while (size > 0)
     {
-        return (NULL);
-    }
-    addr = (char*)(buf_addr + *offset);
-    size = (size_t)(buf_size - *offset);
-    while (1)
-    {
-        if (*addr == DISABLED_LINE)
+        if (chunk->ptr[0] == DISABLED_LINE)
         {
-            ptr = memchr(addr, '\n', size);
-            if (ptr == NULL)
-            {
-                return (NULL);
-            }
-            delta = (size_t)(ptr - addr);
-            addr = ptr + 1;
-            size -= (delta + 1);
-            (*offset) += (delta + 1);
+            if ((next = memchr(chunk->ptr, '\n', size)) == NULL)
+                return (false);
+            size -= (next - chunk->ptr) + 1;
+            chunk->ptr = next + 1;
         }
-        else if (*addr == '\n')
+        else if (chunk->ptr[0] == '\n')
         {
-            addr++;
-            size--;
-            (*offset)++;
+            size -= 1;
+            chunk->ptr += 1;
         }
-        else if ((ptr = memchr(addr, '\n', size)) == NULL)
+        else if ((next = memchr(chunk->ptr, '\n', size)) == NULL)
         {
-            SET_LINE(*line, addr, (size - *offset));
-            (*offset) = size;
-            break;
+            SET_LINE(*dst, chunk->ptr, size);
+            return (true);
         }
-        else if ((line_size = (size_t)(ptr - addr)) > g_conf.line_max_size)
+        else if ((line_size = next - chunk->ptr) > g_conf.line_max_size)
         {
-            /* DLOG("%.*s", line_size, addr); */
-            addr += line_size + 1;
+            chunk->ptr += line_size + 1;
             size -= line_size + 1;
-            (*offset) += line_size + 1;
         }
         else
         {
-            SET_LINE(*line, addr, line_size);
-            *offset += line_size + 1;
-            break;
-        }
-        if (*offset >= buf_size)
-        {
-            return (NULL);
+            SET_LINE(*dst, chunk->ptr, line_size);
+            chunk->ptr += line_size + 1;
+            return (true);
         }
     }
-    return (line);
+    return (false);
 }
 
 
@@ -95,13 +54,11 @@ int         cmp_line(t_line *l1, t_line *l2)
     int     size;
 
     size = LINE_SIZE(*l1);
+
     if (LINE_SIZE(*l2) != size)
-    {
         ret = 1;
-    }
     else
-    {
         ret = memcmp(LINE_ADDR(*l1), LINE_ADDR(*l2), size);
-    }
+
     return (ret);
 }
