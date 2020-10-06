a56fd3380679d49e1f1bc2ac9770054efc9d92c6
nil0x42 <nil0x42@users.noreply.github.com>
2020-10-06 16:41:39 +0000

    tests: add some command-line option tests

--- test/tests/user-interface.sh
+++ test/tests/user-interface.sh
@@ -11,3 +11,14 @@ set -ve
 
 ! ./duplicut --version > /dev/null
 ./duplicut --version | grep -Eq "duplicut.+github"
+
+# -l max value is 255
+./duplicut /etc/passwd -o /tmp/x -l 255 &> /dev/null
+! ./duplicut /etc/passwd -o /tmp/x -l 256 &> /dev/null
+./duplicut /etc/passwd -o /tmp/x -l 256 2>&1 | grep -q "max value is 255"
+
+# -c can't be used together with -C (lowere/upper case)
+./duplicut /etc/passwd -o /tmp/x -c &> /dev/null
+./duplicut /etc/passwd -o /tmp/x -C &> /dev/null
+! ./duplicut /etc/passwd -o /tmp/x -c -C &> /dev/null
+./duplicut /etc/passwd -o /tmp/x -c -C 2>&1 | grep -q "can't use '--lowercase' and '--uppercase' together"
